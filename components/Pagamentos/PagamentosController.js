f44ea27f094bd30e9b86be9a6720ffcf2685541a
Gustavo <gustavo.santos@berghem.com.br>
2019-09-20 13:41:19 -0300

    Corrigindo cache no get e set Pagamentos, deixando os dois funcionando com set e get do redis

--- components/Pagamentos/PagamentosController.js
+++ components/Pagamentos/PagamentosController.js
@@ -7,7 +7,6 @@ const pagamentosService = new PagamentosService();
 module.exports = class PagamentosController {
   static async getPagamentos(req,res){
     try{
-      //cache.del("pagamentos");
       cache.get("pagamentos", async (error,reply) => {
         if(reply){
           console.log('Processando uma requisicao: Retornar todos pagamentos em cache');
@@ -36,11 +35,13 @@ module.exports = class PagamentosController {
       await pagamentosService.setPagamento(pagamento);
   
       cache.get("pagamentos", async (error, reply) => {
-        const testee = JSON.parse(reply);
-        const teste = { ...testee, ...pagamento,  }
-        const carai = JSON.stringify(teste);
-        cache.set("pagamentos", carai);
-        cache.get("pagamentos", async(error, reply) => console.log('NOVO: ', reply));
+        if(reply){
+          const arrayPagamentos = JSON.parse(reply);
+          arrayPagamentos.push(pagamento);
+          cache.set("pagamentos", JSON.stringify(arrayPagamentos));
+        }else{
+          cache.set("pagamentos", reply);
+        }
       });
       
       console.log('Status: Pagamento cadastrado');
@@ -54,7 +55,6 @@ module.exports = class PagamentosController {
     try{
       console.log('Processando uma requisicao: Retornando pagamento especifico');
       const { id } = req.params;
-  
       const pagamento = await pagamentosService.getPagamento(id);
       res.status(200).json(pagamento);
     }catch(error){
